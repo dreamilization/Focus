9728d3553c4ce990e0ad17a9106841fc2e002732
William Wu <chengfeiwu@berkeley.edu>
2019-10-14 00:08:13 -0400

    Layout and animation improvements.

--- Focus/ViewController.swift
+++ Focus/ViewController.swift
@@ -15,25 +15,28 @@ class ViewController: UIViewController {
     @IBOutlet weak var setupButton: UIButton!
     @IBOutlet weak var setupButtonConstraintY: NSLayoutConstraint!
     @IBOutlet var splashScreen: UIView!
+    @IBOutlet weak var splashWelcome: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         if(UserDefaults.standard.bool(forKey: "launchedBefore"))
         {
-            splashTitle.text = "EEE";
+            UIView.animate(withDuration: 0.75, animations: {
+                self.splashWelcome.alpha = 1;
+            })
         }
         else
         {
             UIView.animate(withDuration: 1, delay: 0.5, animations: {
                 self.splashTitle.font = self.splashTitle.font.withSize(30);
                 self.splashTitle.text = "Welcome to Focus";
-                self.splashTitleConstraintY.constant -= 50;
-                self.splashTitle.center.y -= 50;
+                self.splashTitleConstraintY.constant -= 25;
+                self.splashTitle.center.y -= 25;
             })
             UIView.animate(withDuration: 0.75, delay: 0.75, animations: {
-                self.setupButtonConstraintY.constant += 50;
-                self.setupButton.center.y += 50;
+                self.setupButtonConstraintY.constant += 25;
+                self.setupButton.center.y += 25;
                 self.setupButton.alpha = 1;
             }, completion: { (finished: Bool) in
                 self.setupButton.isEnabled = true;
@@ -43,8 +46,9 @@ class ViewController: UIViewController {
 
     @IBAction func setupButtonOnClick(_ sender: UIButton) {
         setupButton.isEnabled = false;
-        UIView.animate(withDuration: 0.75, delay: 0.25, animations: {
-            self.splashScreen.alpha = 0;
+        UIView.animate(withDuration: 0.5, delay: 0.25, animations: {
+            self.splashTitle.alpha = 0;
+            self.setupButton.alpha = 0;
         }, completion: { (finished: Bool) in
             let setupView = self.storyboard?.instantiateViewController(withIdentifier: "SetupViewController") as! SetupViewController;
             self.present(setupView, animated: true, completion: nil);
